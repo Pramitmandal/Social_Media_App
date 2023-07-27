b8c53958361b5ff9d8c0434683f695864d9c8122
Pramitmandal <78475081+Pramitmandal@users.noreply.github.com>
2023-07-28 01:15:01 +0530

    Index Update
    Index Update

--- server/index.js
+++ server/index.js
@@ -3,18 +3,17 @@ import bodyParser from "body-parser";
 import mongoose from "mongoose";
 import dotenv from "dotenv";
 import AuthRoute from "./Routes/AuthRoute.js";
-import PostRoute from './Routes/PostRoute.js';
+import UserRoute from "./Routes/UserRoute.js";
+import PostRoute from "./Routes/PostRoute.js";
 
-//Router
+//Routes
 
 const app = express();
 
-//Middleware
+//MiddleWare
 app.use(bodyParser.json({ limit: "30mb", extended: true }));
 app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
-
 dotenv.config();
-
 mongoose
   .connect(process.env.MONGO_DB, {
     useNewUrlParser: true,
@@ -27,7 +26,7 @@ mongoose
   )
   .catch((error) => console.log(error));
 
-
-  //Usage of routes
-  app.use('/auth', AuthRoute)
-  app.use('/post', PostRoute)
+//usage of routes
+app.use("/auth", AuthRoute);
+app.use("/user", UserRoute);
+app.use('/post', PostRoute);
